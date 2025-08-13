0158c78218f7d811f59d3c7cbd8db8c7bc502c4d
Daniel Moran <d.moran@aims.gov.au>
2025-08-13 14:30:34 +1000

    tidied the conflict

--- test_script.R
+++ test_script.R
@@ -1,9 +1,7 @@
 #initial commit
 #change 1 new line in the main branch
 #change 2 new line in the test branch staged committed and pushed
-<<<<<<< HEAD
 #change 3 new line in the test branch committed only and change branches
-=======
-
 #change 4 new line added via github while the repo is being edited remotely
->>>>>>> e587684030aa99b04c2b77570b0b12176a614958
+#change 5 tidied the conflict and commit then push the merged script
+
